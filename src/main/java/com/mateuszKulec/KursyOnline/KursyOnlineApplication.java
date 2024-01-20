a14cc541fe101b4af8f91c4e80942ede5003b78e
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-01-20 15:23:36 +0100

    Pierwszy Commit, utworzenie projektu z pomoca start.sprin.io i dodaniu do nich zaleznosciu jakie na pewno nam sie przydadza(lista moze zostac rozszerzona w przyszlosci, wszystko znajduje sie w pliku pom.xml

---
+++ src/main/java/com/mateuszKulec/KursyOnline/KursyOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszKulec.KursyOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursyOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursyOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuszKulec/KursyOnline/KursyOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszKulec.KursyOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursyOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
